c66aae687415afb941de5d79d6bfd0e63c77f652
Chris Shih <chs820@gmail.com>
2024-03-10 21:24:38 -0400

    remove create_tables.sql

--- postgres/sql/create_tables.sql
+++
@@ -1,8 +0,0 @@
--- Create test table
-create table if not exists rpt_community_stats_daily (
-    id int not null,
-    date_day date not null,
-    url varchar(250) not null,
-    page_views int not null,
-    primary key (id)
-);
