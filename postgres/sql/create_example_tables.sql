35092037b31e78241d9fd2ee208290312444cb9a
Chris Shih <chs820@gmail.com>
2024-03-26 16:56:52 -0400

    add dynamic time range elements

--- postgres/sql/create_example_tables.sql
+++ postgres/sql/create_example_tables.sql
@@ -9,14 +9,25 @@ create table if not exists rpt_test_data (
 
 copy rpt_test_data from '/usr/share/community_data/example_data.csv' delimiter ',' csv header;
 
--- Create table for overall community stats
-create table if not exists rpt_community_stats_daily (
+-- Create table for individual community page stats
+create table if not exists rpt_community_page_stats_daily (
     received_date date not null,
     page_path varchar(1000) not null,
     page_title varchar(1000),
     is_page_first_day boolean,
     page_views int not null,
     unique_visitors int not null
+);
+
+copy rpt_community_page_stats_daily from '/usr/share/community_data/community_page_stats_daily.csv' delimiter ',' csv header;
+
+-- Create table for overall community stats
+create table if not exists rpt_community_stats_daily (
+    received_date date not null,
+    page_views int not null,
+    new_page_views int not null,
+    unique_visitors int not null,
+    unique_pages int not null
 );
 
 copy rpt_community_stats_daily from '/usr/share/community_data/community_stats_daily.csv' delimiter ',' csv header;
