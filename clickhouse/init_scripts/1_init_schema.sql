41d88abb56298a2bbd1d83b6f2165ac48e463193
Chris Shih <chs820@gmail.com>
2024-03-31 18:50:46 -0400

    add new datasets - page embeddings, prev/next pages

--- clickhouse/init_scripts/1_init_schema.sql
+++ clickhouse/init_scripts/1_init_schema.sql
@@ -3,4 +3,10 @@ CREATE TABLE rpt_community_stats_daily ( `received_date` Date, `page_views` UInt
 DROP TABLE IF EXISTS rpt_community_page_stats_daily;
 CREATE TABLE rpt_community_page_stats_daily ( `received_date` Date, `page_path` String, `page_title` String, `is_page_first_day` Boolean, `page_views` UInt32,  `unique_visitors` UInt32) ENGINE = MergeTree PRIMARY KEY (received_date, page_path);
 DROP TABLE IF EXISTS dim_community_pages;
-CREATE TABLE dim_community_pages ( `page_path` String, `page_title` String, `first_page_date` Date, `t30d_page_views` UInt32,  `t30d_page_rank` UInt32) ENGINE = MergeTree PRIMARY KEY (page_path, page_title);
+CREATE TABLE dim_community_pages ( `page_path` String, `page_title` String, `first_page_date` Date, `total_page_views` UInt32, `t30d_page_views` UInt32,  `t30d_page_rank` UInt32) ENGINE = MergeTree PRIMARY KEY (page_path, page_title);
+DROP TABLE IF EXISTS dim_community_page_embeddings;
+CREATE TABLE dim_community_page_embeddings ( `page_path` String, `page_title` String, `t30d_page_views` UInt32,  `x` Float32, `y` Float32, `z` Float32) ENGINE = MergeTree PRIMARY KEY (page_path, page_title);
+DROP TABLE IF EXISTS rpt_community_previous_pages;
+CREATE TABLE rpt_community_previous_pages ( `page_path` String, `prev_page_path` String, `page_views` UInt32) ENGINE = MergeTree PRIMARY KEY (page_path);
+DROP TABLE IF EXISTS rpt_community_next_pages;
+CREATE TABLE rpt_community_next_pages ( `page_path` String, `next_page_path` String, `page_views` UInt32) ENGINE = MergeTree PRIMARY KEY (page_path);
--- clickhouse/init_scripts/2_insert_data.sh
+++ clickhouse/init_scripts/2_insert_data.sh
@@ -3,3 +3,6 @@
 clickhouse-client -q "INSERT INTO rpt_community_stats_daily FORMAT CSVWithNames" </home/community_data/community_stats_daily.csv
 clickhouse-client -q "INSERT INTO rpt_community_page_stats_daily FORMAT CSVWithNames" </home/community_data/community_page_stats_daily.csv
 clickhouse-client -q "INSERT INTO dim_community_pages FORMAT CSVWithNames" </home/community_data/dim_community_pages.csv
+clickhouse-client -q "INSERT INTO dim_community_page_embeddings FORMAT CSVWithNames" </home/community_data/community_page_embedding.csv
+clickhouse-client -q "INSERT INTO rpt_community_previous_pages FORMAT CSVWithNames" </home/community_data/community_previous_pages.csv
+clickhouse-client -q "INSERT INTO rpt_community_next_pages FORMAT CSVWithNames" </home/community_data/community_next_pages.csv
