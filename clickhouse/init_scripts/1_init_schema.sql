fbc429c76a132a1cc73974fdea11aa2c18cb827f
Chris Shih <chs820@gmail.com>
2024-03-30 22:13:48 -0400

    add dim_community_pages

--- clickhouse/init_scripts/1_init_schema.sql
+++ clickhouse/init_scripts/1_init_schema.sql
@@ -2,3 +2,5 @@ DROP TABLE IF EXISTS rpt_community_stats_daily;
 CREATE TABLE rpt_community_stats_daily ( `received_date` Date, `page_views` UInt32, `new_page_views` UInt32, `unique_visitors` UInt32, `unique_pages` UInt32) ENGINE = MergeTree PRIMARY KEY received_date;
 DROP TABLE IF EXISTS rpt_community_page_stats_daily;
 CREATE TABLE rpt_community_page_stats_daily ( `received_date` Date, `page_path` String, `page_title` String, `is_page_first_day` Boolean, `page_views` UInt32,  `unique_visitors` UInt32) ENGINE = MergeTree PRIMARY KEY (received_date, page_path);
+DROP TABLE IF EXISTS dim_community_pages;
+CREATE TABLE dim_community_pages ( `page_path` String, `page_title` String, `first_page_date` Date, `t30d_page_views` UInt32,  `t30d_page_rank` UInt32) ENGINE = MergeTree PRIMARY KEY (page_path, page_title);
--- clickhouse/init_scripts/2_insert_data.sh
+++ clickhouse/init_scripts/2_insert_data.sh
@@ -2,3 +2,4 @@
 
 clickhouse-client -q "INSERT INTO rpt_community_stats_daily FORMAT CSVWithNames" </home/community_data/community_stats_daily.csv
 clickhouse-client -q "INSERT INTO rpt_community_page_stats_daily FORMAT CSVWithNames" </home/community_data/community_page_stats_daily.csv
+clickhouse-client -q "INSERT INTO dim_community_pages FORMAT CSVWithNames" </home/community_data/dim_community_pages.csv
